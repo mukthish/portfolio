53585e681964948318854ef2b6d1e3071d357a6c
psentaur <mukthish.2000@gmail.com>
2024-06-05 11:05:27 +0530

    updated information

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,4 @@
 import React, { Component } from "react";
-import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
-import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
 
 class About extends Component {
     render() {
@@ -29,27 +25,6 @@ class About extends Component {
                                         height="250px"
                                         src={profilepic}
                                         alt="Avatar placeholder"
-                                    />
-                                    <Icon
-                                        icon={angularIcon}
-                                        style={{
-                                            fontSize: "400%",
-                                            margin: "9% 5% 0 5%",
-                                        }}
-                                    />
-                                    <Icon
-                                        icon={reactIcon}
-                                        style={{
-                                            fontSize: "400%",
-                                            margin: "9% 5% 0 5%",
-                                        }}
-                                    />
-                                    <Icon
-                                        icon={vueIcon}
-                                        style={{
-                                            fontSize: "400%",
-                                            margin: "9% 5% 0 5%",
-                                        }}
                                     />
                                 </span>
                             </div>
